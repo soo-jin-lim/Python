7b8131c52aa514eb475f111cb1c22263040646ba
임수진 <123914453+soo-jin-lim@users.noreply.github.com>
2023-11-14 10:25:49 +0900

    .수정
    .

--- chap07/Code07-07.py
+++ chap07/Code07-07.py
@@ -3,14 +3,16 @@ import csv
 import os
 
 csvFileList = ["../chap06/CSV/singerA.csv", "../chap06/CSV/singerB.csv"]
-outWorkbook = xlwt.Workbook()
+outWorkbook = xlwt.Workbook() #엑셀 파일로 출력하기 위한 객체
 
 for csvFileName in csvFileList :
     rowCount = 0
-    with open(csvFileName, "r") as inFp:
-        csvReader = csv.reader(inFp)
-        header_list = next(csvReader)
+    with open(csvFileName, "r") as inFp:#파일을 읽어들인 객체
+        csvReader = csv.reader(inFp)#csv 파일의 첫 행 읽기
+        header_list = next(csvReader)#csv 파일의 첫행 읽기
+        #os.path.basename 는()경로에서 파일명만 추출
         outSheet = outWorkbook.add_sheet(os.path.basename(csvFileName))
+        #제목만 추출해서 입력
         for col in range(len(header_list)) :
             outSheet.write(rowCount, col, header_list[col])
         for row_list in csvReader:
