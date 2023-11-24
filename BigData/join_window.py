6c366f427af8b339181384d4c7280a434068baf9
임수진 <123914453+soo-jin-lim@users.noreply.github.com>
2023-11-24 11:55:09 +0900

    .

--- BigData/join_window.py
+++ BigData/join_window.py
@@ -5,7 +5,6 @@ def registration(registration_window):
     name_var, phone_var, email_var, id_var, password_var, confirm_password_var = StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar()
 
     def validate_input():
-        # 입력 필드에서 값 가져오기
         name = name_var.get()
         phone_number = phone_var.get()
         email = email_var.get()
@@ -13,17 +12,12 @@ def registration(registration_window):
         password = password_var.get()
         confirm_password = confirm_password_var.get()
 
-        # 모든 필드가 채워져 있는지 확인
         if name and phone_number and email and user_id and password and confirm_password:
             if password == confirm_password:
-                # 모든 정보가 입력되고 비밀번호도 일치하는 경우
                 tk.Label(registration_window, text="회원가입 성공", fg="green").grid(row=8, column=0, columnspan=2, pady=10)
-                # 여기에 추가적인 처리를 할 수 있습니다.
             else:
-                # 비밀번호가 일치하지 않는 경우
                 tk.Label(registration_window, text="비밀번호가 일치하지 않습니다", fg="red").grid(row=8, column=0, columnspan=2, pady=10)
         else:
-            # 빈 칸이 있는 경우
             tk.Label(registration_window, text="모든 정보를 다 입력하세요", fg="red").grid(row=8, column=0, columnspan=2, pady=10)
 
     registration_window.title("회원가입 창")
